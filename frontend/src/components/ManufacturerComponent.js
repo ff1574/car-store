4a6a5132c82c21ebf51d54e78d39b727c4064091
Denny Lulak <dal4933@rit.edu>
2024-04-22 22:18:43 +0200

    Frontend

--- frontend/src/components/ManufacturerComponent.js
+++ frontend/src/components/ManufacturerComponent.js
@@ -26,15 +26,49 @@ function ManufacturerComponent() {
     navigate(`/manufacturers/${manufacturerId}`);
   };
 
+  const manufacturersListStyle = {
+    display: "flex", // Use flexbox to lay out the cards horizontally
+    flexWrap: "wrap", // Allow wrapping of cards if there's not enough horizontal space
+    justifyContent: "center", // Center-align the cards horizontally
+    alignItems: "center", // Center-align the cards vertically
+    gap: "20px", // Add space between the cards
+  };
+
+  const manufacturerCardStyle = {
+    cursor: "pointer",
+    textAlign: "center", // Center-align all contents of the card
+    marginBottom: "20px", // Add some margin at the bottom for spacing
+    paddingRight: "40px",
+  };
+
+  const manufacturerImageStyle = {
+    width: "100px", // Smaller width for images
+    height: "100px", // Set height to maintain aspect ratio, adjust as needed
+    marginBottom: "10px", // Space between image and name
+  };
+
+  const manufacturerNameStyle = {
+    fontSize: "30px", // Larger font size for names
+    fontWeight: "bold", // Optional: make the font weight bold
+  };
+  const headingStyle = {
+    textAlign: "center",
+    fontSize: "60px", // Larger font size for names
+    marginTop: "150px",
+    marginBottom: "100px",
+    color: "#52abff", // Apply the previously used blue color
+    margin: "20px 0",
+  };
+
   return (
     <div>
-      <h1>Manufacturers</h1>
-      <div className="manufacturers-list">
+      <h1 style={headingStyle}>Manufacturers</h1>
+      <div style={manufacturersListStyle}>
         {manufacturers.length > 0 ? (
           manufacturers.map((manufacturer) => (
             <div
               key={manufacturer.manufacturerId}
-              className="manufacturer-card"
+              style={manufacturerCardStyle}
               onClick={() =>
                 handleManufacturerClick(manufacturer.manufacturerId)
               }
@@ -42,9 +76,11 @@ function ManufacturerComponent() {
               <img
                 src={`data:image/png;base64,${manufacturer.manufacturerImage}`}
                 alt={`${manufacturer.manufacturerName} Logo`}
-                className="manufacturer-logo"
+                style={manufacturerImageStyle}
               />
-              <h2>{manufacturer.manufacturerName}</h2>
+              <h2 style={manufacturerNameStyle}>
+                {manufacturer.manufacturerName}
+              </h2>
             </div>
           ))
         ) : (
